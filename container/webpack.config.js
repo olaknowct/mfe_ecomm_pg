4016f87544ff292fd9d641d620cb2436dfbce688
Christopher Olano <olanochristopher@gmail.com>
2024-06-04 21:56:55 +0800

    fix and add missing package (module federation)

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 module.exports = {
   mode: 'development',
