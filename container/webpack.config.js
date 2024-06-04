1ee83c2c8eed444f07f88e02d9d03ad4fbb9a219
Christopher Olano <olanochristopher@gmail.com>
2024-06-04 17:13:04 +0800

    implementing module federation

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -6,6 +6,14 @@ module.exports = {
     port: 8080,
   },
   plugins: [
+    // it is use to defined which files you want to get from the remote
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        // alias - (name of the module @ link)
+        products: 'products@http://localhost:8081/remoteEntry.js',
+      },
+    }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
--- products/webpack.config.js
+++ products/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 module.exports = {
   mode: 'development',
@@ -6,6 +7,14 @@ module.exports = {
     port: 8081,
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: 'products',
+      filename: 'remoteEntry.js',
+      exposes: {
+        // alias - directory
+        './ProductsIndex': './src/index',
+      },
+    }),
     // checks every thing webpack build and add it automatically into the template
     new HtmlWebpackPlugin({
       template: './public/index.html',
