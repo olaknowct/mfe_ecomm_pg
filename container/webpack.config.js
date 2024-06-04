2d85fbfe74a90826e943ada81effead8946ef572
Christopher Olano <olanochristopher@gmail.com>
2024-06-04 21:57:26 +0800

    cart integration with container

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
       remotes: {
         // alias - (name of the module @ link)
         products: 'products@http://localhost:8081/remoteEntry.js',
+        cart: 'cart@http://localhost:8082/remoteEntry.js',
       },
     }),
     new HtmlWebpackPlugin({
